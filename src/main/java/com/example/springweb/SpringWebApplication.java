ada9bad0a6e46cd704bffe982478748587b1bb95
furssov <fursovd70@gmail.com>
2022-07-29 11:39:31 +0300

    start project

---
+++ src/main/java/com/example/springweb/SpringWebApplication.java
@@ -0,0 +1,43 @@
+package com.example.springweb;
+
+import com.mysql.jdbc.Driver;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.*;
+import java.util.Properties;
+
+@SpringBootApplication
+public class SpringWebApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringWebApplication.class, args);
+        /*
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:mysql://localhost:3306/people?autoReconnect=true&useSSL=false";
+        String user = "root";
+        String password = "passwordWOW12!!@@";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement()) {
+            String sql = "SELECT * FROM phonebook";
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name") +
+                        " " + resultSet.getInt("number"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+         */
+
+    }
+
+}
